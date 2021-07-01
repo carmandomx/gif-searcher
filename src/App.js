50da6de032ab22881f8b50cc3fa56cbe6d77afd3
Carlos Reyes <carmandoreyes93@gmail.com>
2021-06-30 21:31:48 -0500

    gif

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
+import searchGif from './services/searchGif'
+import Gif from './components/Gif'
+import Search from './components/Search'
 
-function App() {
+function App () {
+  const [currentGif, setCurrentGif] = useState({
+    images: {
+      original: {
+        url: ''
+      }
+    }
+  })
+  const [queryTerm, setQueryTerm] = useState('puppies')
+  useEffect(() => {
+    searchGif(queryTerm).then(data => {
+      setCurrentGif(data.data[0])
+    })
+  }, [queryTerm])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Search changeQuery={setQueryTerm} />
+        <Gif url={currentGif.images.original.url} />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
